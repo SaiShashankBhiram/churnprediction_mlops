737682f98466baacfcf96caea0bca4ae7296b64e
SaiShashankBhiram <saishashankbhiram@gmail.com>
2025-04-19 13:35:12 +0530

    Data Validation completed

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from churnprediction.components.data_ingestion import DataIngestion
 from churnprediction.exception.exception import ChurnPredictionException
-
 from churnprediction.logging.logger import logging
-from churnprediction.entity.config_entity import DataIngestionConfig
+from churnprediction.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from churnprediction.entity.config_entity import TrainingPipelineConfig
+from churnprediction.components.data_validation import DataValidation
 
+import sys
 
 if __name__=='__main__':
     try:
@@ -13,7 +14,15 @@ if __name__=='__main__':
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("Initiate the data Ingestion")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation completed")
         print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, data_validation_config)
+        logging.info("Initiate the data validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("data validation completed")
+        print(data_validation_artifact)
 
     except Exception as e:
-           raise ChurnPredictionException(e,sys)
+            raise ChurnPredictionException(e,sys)
+    
